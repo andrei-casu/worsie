662f7fb921ba3b91dc09e9b8900cb3f81daf614d
AndreiTi <andrei.timofte96@gmail.com>
2018-04-01 18:40:36 +0300

    made /events responsive

--- frontend/src/components/pages/Header.js
+++ frontend/src/components/pages/Header.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Logo from '../../images/logo.png';
 
-export default class Header extends Component{
+export default class Header extends Component {
 
-	render(){
-		const {user} = this.props;
 
-		return(
-			<div className="header-content">
+	constructor(props) {
+		super(props);
 
-				<div className="header-content-logo"> <img src={Logo}/> </div>
 
-				<NavLink className="header-content-link" to="/events/main"><i className="fas fa-home"/></NavLink>
+		this.state = {
+			showMenu: false
+		};
+		this.menuClick = this.menuClick.bind(this);
+
+	}
+
+	componentWillReceiveProps(props){
+		this.setState({showMenu: false});
+	}
+
+	menuClick() {
+		this.setState({ showMenu: !this.state.showMenu });
+	}
+
+	render() {
+		const { user } = this.props;
+		const { showMenu } = this.state;
+
+		return (
+			<div className={`header-content ${showMenu === true && "responsive"}`}>
+
+				<div className="header-content-logo"> <img src={Logo} /> </div>
+
+				<NavLink className="header-content-link" to="/events/main"><i className="fas fa-home" /></NavLink>
 				<NavLink className="header-content-link" to="/events/3_hours">Next 3 hours</NavLink>
 				{
 					[1, 2, 3, 4, 5, 6, 7].map((nr) => (
 						<NavLink key={nr} className="header-content-link" to={`/events/${nr}`}> {nr} day(s)</NavLink>
 					))
 				}
-				
+
+				<a className="header-content-link icon"> <i className="fas fa-bars" onClick={this.menuClick}></i> </a>
+
+
+
 				<div className="user-info">
 					<div className="user-name">{user.name}</div>
-					<img className="user-avatar" src={user.avatar}/>
+					<img className="user-avatar" src={user.avatar} />
 				</div>
 			</div>
 		);
