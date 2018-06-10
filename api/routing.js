3a8ee1847504aa06c8ddae27e9c55fcc47c857c4
Andrei <casuneanu.andrei97@gmail.com>
2018-06-10 17:25:23 +0300

    added cors

--- api/routing.js
+++ api/routing.js
@@ -39,7 +39,7 @@ module.exports = (() => {
         }
       }
       
-      response.writeHead(200);
+      response.writeHead(200, {'Access-Control-Allow-Origin': '*'});
       response.end();
     };
 
